9f1fe6ecba3749ed49adc914b9fac19a9c5abc12
Taras Tsalko <you@example.com>
2025-07-31 15:21:37 +0300

    Реализован класс thread_pool

--- include/thread_pool/thread_pool.hpp
+++ include/thread_pool/thread_pool.hpp
@@ -1,9 +1,32 @@
 #pragma once
 
+#include <memory>
+#include <thread>
+#include <vector>
+
+namespace dispatcher::queue {
+
+class PriorityQueue;
+
+}  // namespace dispatcher::queue
+
 namespace dispatcher::thread_pool {
 
 class ThreadPool {
-  // здесь ваш код
+
+public:
+    explicit ThreadPool(std::shared_ptr<dispatcher::queue::PriorityQueue> queue, size_t numThreads);
+
+    ~ThreadPool();
+
+private:
+    void RunTasks();
+
+private:
+    std::shared_ptr<dispatcher::queue::PriorityQueue> queue_;
+    const size_t numThreads_;
+    std::vector<std::thread> threads_;
+    std::mutex m_;
 };
 
-} // namespace dispatcher::thread_pool
+}  // namespace dispatcher::thread_pool
--- src/thread_pool/thread_pool.cpp
+++ src/thread_pool/thread_pool.cpp
@@ -1,7 +1,35 @@
 #include "thread_pool/thread_pool.hpp"
 
+#include "queue/priority_queue.hpp"
+
 namespace dispatcher::thread_pool {
 
 // здесь ваш код
+ThreadPool::ThreadPool(std::shared_ptr<dispatcher::queue::PriorityQueue> queue, size_t numThreads)
+    : queue_(queue), numThreads_(numThreads) {
+    // Создаем и запускаем рабочие потоки
+    for (size_t i = 0; i < numThreads_; ++i) {
+        threads_.emplace_back(&ThreadPool::RunTasks, this);
+    }
+}
 
-} // namespace dispatcher::thread_pool
+ThreadPool::~ThreadPool() {
+    // Ожидаем завершения всех потоков
+    for (auto &thread : threads_) {
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
+}
+
+void ThreadPool::RunTasks() {
+    while (true) {
+        auto task = queue_->pop();
+        if (!task.has_value()) {
+            break;  // // Завершаем работу, если получили nullopt
+        }
+        std::invoke(task.value());
+    }
+}
+
+}  // namespace dispatcher::thread_pool
