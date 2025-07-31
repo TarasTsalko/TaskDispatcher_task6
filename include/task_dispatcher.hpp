298572ea61689d2149224ac1260f42e4bdf3f0c6
Taras Tsalko <you@example.com>
2025-07-31 15:25:23 +0300

    Реализован класс TaskDispatcher

--- include/task_dispatcher.hpp
+++ include/task_dispatcher.hpp
@@ -11,10 +11,16 @@ namespace dispatcher {
 class TaskDispatcher {
     // здесь ваш код
 public:
-    // TaskDispatcher(size_t thread_count, ?);
+    TaskDispatcher(size_t thread_count, const queue::QueueOptions &highPriorityOption = {true, 1000},
+                   const queue::QueueOptions &normalPriorityOption = {false, 0});
 
     void schedule(TaskPriority priority, std::function<void()> task);
+
     ~TaskDispatcher();
+
+private:
+    std::shared_ptr<queue::PriorityQueue> priorityQueue_;
+    thread_pool::ThreadPool thread_pool_;
 };
 
 }  // namespace dispatcher
--- src/task_dispatcher.cpp
+++ src/task_dispatcher.cpp
@@ -2,6 +2,17 @@
 
 namespace dispatcher {
 
+TaskDispatcher::TaskDispatcher(size_t thread_count, const queue::QueueOptions &highPriorityOption,
+                               const queue::QueueOptions &normalPriorityOption)
+    : priorityQueue_(new queue::PriorityQueue(highPriorityOption, normalPriorityOption)),
+      thread_pool_(priorityQueue_, thread_count) {}
+
+void TaskDispatcher::schedule(TaskPriority priority, std::function<void()> task) {
+    priorityQueue_->push(priority, std::move(task));
+}
+
+TaskDispatcher::~TaskDispatcher() { priorityQueue_->shutdown(); }
+
 // здесь ваш код
 
-} // namespace dispatcher
+}  // namespace dispatcher
